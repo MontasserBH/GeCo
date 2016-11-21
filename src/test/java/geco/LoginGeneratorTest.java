35e5a0956a9b8f2f1c2ece4dc9794dab371d54cf
MontasserBH <montasserb94@gmail.com>
2016-11-22 00:19:21 +0100

    GeCo-2: Couvrir LoginGenerateur à 100 % par des tests unitaires #2
    fix #3

---
+++ src/test/java/geco/LoginGeneratorTest.java
@@ -0,0 +1,45 @@
+package geco;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by MONTASSER on 22/11/2016.
+ */
+public class LoginGeneratorTest {
+    private LoginGenerator aLoginGenerateur;
+
+    @Before
+    public void setUp() throws Exception {
+        String[] start = new String[]{"JROL", "BPER", "CGUR", "JDUP", "JRAL", "JRAL1"};
+        aLoginGenerateur = new LoginGenerator(new LoginService(start));
+    }
+
+    @Test
+    public void generateLoginForNomAndPrenom() throws Exception {
+        String login = aLoginGenerateur.generateLoginForNomAndPrenom("Durand", "Paul");
+        Assert.assertEquals("PDUR", login);
+    }
+
+    @Test
+    public void generateLoginForJRAL2() throws Exception {
+        String login = aLoginGenerateur.generateLoginForNomAndPrenom("Ralling", "John");
+        Assert.assertEquals("JRAL2",login);
+    }
+
+    @Test
+    public void generateLoginForJROL1() throws Exception {
+        String login = aLoginGenerateur.generateLoginForNomAndPrenom("Rolling", "Jean");
+        Assert.assertEquals("JROL1",login);
+    }
+
+    @Test
+    public void generateLoginForPDUR() throws Exception {
+        String login = aLoginGenerateur.generateLoginForNomAndPrenom("Dùrand", "Paul");
+        Assert.assertEquals("PDUR", login);
+    }
+
+}
